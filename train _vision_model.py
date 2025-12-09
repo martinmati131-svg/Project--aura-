b207af22ea767902a6409d1c9c20f957b562a25c
martinmati131-svg <martinmati131@gmail.com>
2025-12-09 13:53:49 +0300

    Create train _vision_model.py

---
+++ train _vision_model.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# ... (Your CNN model definition goes here) ...
+
+# This automatically loads images from the folders we just created
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    'vision_dataset',
+    target_size=(64, 64), # Must match the collector resize
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator = datagen.flow_from_directory(
+    'vision_dataset',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Train using the generators
+model.fit(train_generator, epochs=10, validation_data=validation_generator)
